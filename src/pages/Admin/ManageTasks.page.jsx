6049734cc7c78b2e2965191d31f80026a0d9a965
itz-Hiru <hirumithagithub@gmail.com>
2025-04-16 20:10:55 +0530

    Created manage tasks

--- src/pages/Admin/ManageTasks.page.jsx
+++ src/pages/Admin/ManageTasks.page.jsx
@@ -1,17 +1,112 @@
-import React, { useContext } from 'react';
-import DashboardLayout from '../../components/Layouts/DashboardLayout.component';
-import { UserContext } from '../../context/userContext.context';
-import { useUserAuth } from '../../hooks/useUserAuth.hook';
+import React, { useContext, useEffect, useState } from "react";
+import { LuFileSpreadsheet } from "react-icons/lu";
+import { useNavigate } from "react-router-dom";
+import DashboardLayout from "../../components/Layouts/DashboardLayout.component";
+import TaskStatusTabs from "../../components/Tabs/TaskStatusTabs.component";
+import { useUserAuth } from "../../hooks/useUserAuth.hook";
+import { API_PATHS } from "../../utils/apiPath.util";
+import axiosInstance from "../../utils/axiosInstance.util";
+import TaskCard from "../../components/Cards/TaskCard.component";
 
 const ManageTasks = () => {
   useUserAuth();
-  
-    const { user } = useContext(UserContext);
+  const [allTasks, setAllTasks] = useState([]);
+  const [tabs, setTabs] = useState([]);
+  const [filterStatus, setFilterStatus] = useState("All");
+  const navigate = useNavigate();
+
+  const getAllTasks = async (status) => {
+    try {
+      const response = await axiosInstance.get(API_PATHS.TASKS.GET_ALL_TASKS, {
+        params: {
+          status: status === "All" ? "" : status,
+        },
+      });
+
+      setAllTasks(response.data?.tasks?.length > 0 ? response.data.tasks : []);
+
+      const statusSummary = response.data?.statusSummary || {};
+
+      const statusArray = [
+        { label: "All", count: statusSummary.all || 0 },
+        { label: "Pending", count: statusSummary.pendingTasks || 0 },
+        { label: "In Progress", count: statusSummary.inProgressTasks || 0 },
+        { label: "Completed", count: statusSummary.completedTasks || 0 },
+      ];
+
+      setTabs(statusArray);
+    } catch (e) {
+      console.error("Error fetching tasks", e);
+    }
+  };
+
+  const handleClick = (taskData) => {
+    navigate("/admin/create-task", { state: { taskId: taskData._id } });
+  };
+
+  // Download task report
+  const handleDownloadReport = async () => {};
+
+  useEffect(() => {
+    getAllTasks(filterStatus);
+  }, [filterStatus]);
   return (
     <DashboardLayout activeMenu="Manage Tasks">
+      <div className="my-5">
+        <div className="flex flex-col lg:flex-row lg:items-center justify-between">
+          <div className="flex items-center justify-between gap-3 ">
+            <h2 className="text-xl md:text-xl font-medium">My Tasks</h2>
+            <button
+              className="flex lg:hidden download-btn"
+              onClick={handleDownloadReport}
+            >
+              <LuFileSpreadsheet className="text-lg" />
+              Download Report
+            </button>
+          </div>
 
+          {tabs?.[0]?.count > 0 && (
+            <div className="flex items-center gap-3">
+              <TaskStatusTabs
+                tabs={tabs}
+                activeTab={filterStatus}
+                setActiveTab={setFilterStatus}
+              />
+              <button
+                className="hidden lg:flex download-btn"
+                onClick={handleDownloadReport}
+              >
+                <LuFileSpreadsheet className="text-lg" />
+                Download Report
+              </button>
+            </div>
+          )}
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-4">
+          {allTasks?.map((item) => (
+            <TaskCard
+              key={item._id}
+              title={item.title}
+              description={item.description}
+              priority={item.priority}
+              status={item.status}
+              progress={item.progress}
+              createdAt={item.createdAt}
+              dueDate={item.dueDate}
+              assignedTo={item.assignedTo?.map((item) => item.profileImageUrl)}
+              attachmentCount={item.attachments?.length || 0}
+              completedTodoCount={item.completedTodoCount || 0}
+              todoChecklist={item.todoChecklist || []}
+              onClick={() => {
+                handleClick(item);
+              }}
+            />
+          ))}
+        </div>
+      </div>
     </DashboardLayout>
-  )
-}
+  );
+};
 
-export default ManageTasks
+export default ManageTasks;
